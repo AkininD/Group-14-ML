3b36f44931ce2604bd5de864b24eafdf262d6570
Iremel Iskuzhin <9588504@gmail.com>
2022-11-12 23:40:25 +0500

    add files

--- main.py
+++ main.py
@@ -1,6 +1,70 @@
-from transformers import pipeline
+from PIL import Image
+import requests
+import uuid
+import matplotlib.pyplot as plt
+import torch
+import detrd
+import helpers
+import torchvision.transforms as T
 
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
+torch.set_grad_enabled(False);
 
-print(classifier("Я обожаю инженерию машинного обучения!"))
+detr = detrd.Detr(num_classes=91)
+state_dict = torch.hub.load_state_dict_from_url(
+    url='https://dl.fbaipublicfiles.com/detr/detr_demo-da2a99e9.pth',
+    map_location='cpu', check_hash=True)
+detr.load_state_dict(state_dict)
+detr.eval();
+
+# COCO classes
+CLASSES = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+# url = "http://images.cocodataset.org/test-stuff2017/000000000311.jpg"
+# url = "http://images.cocodataset.org/test-stuff2017/000000000128.jpg"
+url ="http://images.cocodataset.org/test-stuff2017/000000000527.jpg"
+
+im = Image.open(requests.get(url, stream=True).raw)
+
+scores, boxes = helpers.detect(im, detr, transform)
+
+def plot_results(pil_img, prob, boxes):
+    plt.figure(figsize=(16,10))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), COLORS * 100):
+        ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                   fill=False, color=c, linewidth=3))
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+        ax.text(xmin, ymin, text, fontsize=15,
+                bbox=dict(facecolor='yellow', alpha=0.5))
+    plt.axis('off')
+    plt.savefig(f'image_{str(uuid.uuid4())}.jpg')
+
+plot_results(im, scores, boxes)
