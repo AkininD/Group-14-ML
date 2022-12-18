ebbf32c2dc4cc9ef66f4185a8c77b3e07b91311e
Iremel Iskuzhin <9588504@gmail.com>
2022-12-18 14:36:06 +0500

    add tests

---
+++ test_myapi.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from myapi import app
+import json
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+
+def test_predict_cat():
+    filename = "./tests/fixtures/cat.jpg"
+    response = client.post("/predict", files={"file": ("cat.jpg", open(filename, "rb"), "image/jpeg")})
+    predict_response = json.loads(response.content)
+    assert response.status_code == 200
+    assert any('cat' in d for d in predict_response)
+    assert any('image_url' in d for d in predict_response)
+    tear_down()
+
+def test_predict_elephant():
+    filename = "./tests/fixtures/elephant.jpg"
+    response = client.post("/predict", files={"file": ("elephant.jpg", open(filename, "rb"), "image/jpeg")})
+    predict_response = json.loads(response.content)
+    assert response.status_code == 200
+    assert any('elephant' in d for d in predict_response)
+    assert any('image_url' in d for d in predict_response)
+    tear_down()
+
+def tear_down():
+    import os
+    from os import listdir
+    from os.path import isfile, join
+    onlyfiles = [f for f in listdir('./static') if isfile(join('./static', f))]
+    for file in onlyfiles:
+        os.remove(f'./static/{file}')
